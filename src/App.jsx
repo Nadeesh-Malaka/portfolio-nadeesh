6424fb1e17a8907154140a190d8cd8267613a18c
Nadeesh Malaka Chathuranga <Nadeeshmalaka2001@gmail.com>
2025-03-28 00:53:49 +0530

    Add Hero component with styling For Home Page Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/NavBar/Navbar'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div> 
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>Hi, I'm Nadeesh Malaka,</span> Full Stack Developer based in Sri
+        Lanka
+      </h1>
+      <p>
+        I love coding, solving problems, and learning new technologies every
+        day.
+      </p>
+      <div className="hero-actions">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
