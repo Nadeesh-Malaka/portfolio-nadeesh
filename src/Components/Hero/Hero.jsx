bf4ba73a44cbec9949ae2285c1c95034fb5065ca
Nadeesh Malaka Chathuranga <Nadeeshmalaka2001@gmail.com>
2025-03-30 23:35:02 +0530

    Add anchor links and IDs for smooth scrolling navigation in Navbar, About, Contact, Hero, and Mywork components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
         <span>Hi, I'm Nadeesh Malaka,</span> Full Stack Developer based in Sri
@@ -15,7 +16,7 @@ const Hero = () => {
         day.
       </p>
       <div className="hero-actions">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect with me</AnchorLink> </div>
         <div className="hero-resume">My Resume</div>
       </div>
     </div>
