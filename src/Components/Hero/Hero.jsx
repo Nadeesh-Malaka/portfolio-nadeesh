6ac25613bc22c8e957997d858c8adecd44135859
Nadeesh Malaka Chathuranga <Nadeeshmalaka2001@gmail.com>
2025-03-31 03:55:50 +0530

    Add responsive styles for Hero component and enhance accessibility

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import cv_file from  "../../assets/Nadeesh Malaka .pdf"; // Import the CV file
 
 const Hero = () => {
   return (
     <div id="home" className="hero">
-      <img src={profile_img} alt="" />
+      <img src={profile_img} alt="Profile Image" />
       <h1>
         <span>Hi, I'm Nadeesh Malaka,</span> Full Stack Developer based in Sri
         Lanka
@@ -16,8 +17,14 @@ const Hero = () => {
         day.
       </p>
       <div className="hero-actions">
-        <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect with me</AnchorLink> </div>
-        <div className="hero-resume">My Resume</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <a href={cv_file} download="Nadeesh_Malaka_CV.pdf" className="hero-resume">
+          My Resume
+        </a>
       </div>
     </div>
   );
